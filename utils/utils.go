bccc201b1b2baaeaf5a3ae7fef4309ab19805e40
yvan <yvanscher@gmail.com>
2021-12-30 10:43:32 +0100

    update method to be exported moethod

--- utils/utils.go
+++ utils/utils.go
@@ -6,7 +6,7 @@ import (
 )
 
 // little function to print the methods of an object
-func printMethods (object interface{}) {
+func PrintMethods (object interface{}) {
 	oType := reflect.TypeOf(object)
 	for i := 0; i < oType.NumMethod(); i++ {
 		method := oType.Method(i)
